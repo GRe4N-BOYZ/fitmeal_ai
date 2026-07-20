329d184141077c39cb61f36519923384d1bf3955
Ugallun <yonomo.sasakura@gmail.com>
2026-07-20 18:03:12 +0900

    add main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:fitmeal_ai/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FitmealAI());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
